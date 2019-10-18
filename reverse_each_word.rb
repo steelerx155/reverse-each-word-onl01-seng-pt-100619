1ac871bc321cbf7a20e2ed63f7504c613e5f84ee
Randy Steele <steelerx155@gmail.com>
2019-10-18 14:22:18 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -2,5 +2,5 @@ require 'pry'
 def reverse_each_word(sentence1)
   backwards = []
   backwards << sentence1.split
-   sentence1.split.map { |words| words.reverse}.join(" ")
+   sentence1.split.collect { |words| words.reverse}.join(" ")
 end
